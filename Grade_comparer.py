7c2e9e32de8e1ae7a61c7e835b49490157180910
ahua081 <150732795+ahua081@users.noreply.github.com>
2023-12-07 22:38:31 -0600

    Update Grade_comparer.py

--- Grade_comparer.py
+++ Grade_comparer.py
@@ -1,17 +1,11 @@
 from PyQt6.QtWidgets import QDialog, QVBoxLayout, QLabel, QLineEdit, QPushButton
+from typing import Dict
 
 
 class GradeComparerWindow(QDialog):
     def __init__(self, main_window):
-        """
-        Initialize the GradeComparer Window.
-
-        Args:
-        - main_window: Reference to the main GradeAssignerWindow.
-        """
         super(GradeComparerWindow, self).__init__()
 
-        # Initialize the GradeComparer Window
         self.setWindowTitle("Grade Comparer")
         self.setGeometry(200, 200, 400, 300)
 
@@ -19,11 +13,9 @@ class GradeComparerWindow(QDialog):
 
         self.layout = QVBoxLayout()
 
-        # Add a text box saying "Please assign the grades the students got."
         self.instruction_label = QLabel("Please assign the grades the students got.", self)
         self.layout.addWidget(self.instruction_label)
 
-        # Add input boxes for each student to assign grades
         self.grade_inputs = []
         for student_name in self.main_window.students:
             label = QLabel(f"{student_name}:", self)
@@ -36,62 +28,65 @@ class GradeComparerWindow(QDialog):
             self.layout.addWidget(grade_input)
             self.layout.addWidget(percent_label)
 
-        # Add Compare Grades button
         self.compare_button = QPushButton("Compare Grades", self)
         self.compare_button.clicked.connect(self.compare_grades)
         self.layout.addWidget(self.compare_button)
 
-        # Add labels for best and worst grades
         self.best_grade_label = QLabel("", self)
         self.worst_grade_label = QLabel("", self)
+        self.duplicate_grade_label = QLabel("", self)
         self.layout.addWidget(self.best_grade_label)
         self.layout.addWidget(self.worst_grade_label)
+        self.layout.addWidget(self.duplicate_grade_label)
 
         self.setLayout(self.layout)
 
     def compare_grades(self):
-        """
-        Compare grades and update the labels with the best and worst grades.
-        """
-        grades = []
+        grades = [float(grade_input.text().strip() or 0) for grade_input in self.grade_inputs]
 
-        try:
-            for grade_input in self.grade_inputs:
-                grade_text = grade_input.text().strip()
-
-                # Check if the grade is not empty
-                if not grade_text:
-                    raise ValueError("Please enter grades for all students.")
-
-                try:
-                    # Attempt to convert the grade to a float
-                    grade = float(grade_text)
-                except ValueError:
-                    raise ValueError("Please enter valid numerical grades.")
-
-                # Check if the grade is between 0 and 100
-                if not (0 <= grade <= 100):
-                    raise ValueError("Please enter grades between 0 and 100.")
-
-                grades.append(grade)
-
-        except ValueError as e:
-            # Display the specific error message
-            self.best_grade_label.setText(f"Invalid Input: {e}")
+        if any(grade < 0 or grade > 100 for grade in grades):
+            self.best_grade_label.setText("Please enter grades between 0 and 100.")
             self.worst_grade_label.setText("")
+            self.duplicate_grade_label.setText("")
             return
 
-        # Check if there are any grades entered
-        if not grades:
-            self.best_grade_label.setText("No grades entered.")
+        if not all(grades):
+            self.best_grade_label.setText("Please enter grades for all students.")
             self.worst_grade_label.setText("")
+            self.duplicate_grade_label.setText("")
             return
 
-        # Find the student with the highest and lowest grades
+        # Check for duplicate grades
+        grade_count = {}
+        duplicate_messages = []
+
+        for student, grade in zip(self.main_window.students.keys(), grades):
+            if grade in grade_count:
+                grade_count[grade].append(student)
+            else:
+                grade_count[grade] = [student]
+
+        # Filter out grades with only one student
+        duplicate_grades = {grade: students for grade, students in grade_count.items() if len(students) > 1}
+
+        # Display the duplicate grades message only if there are duplicates
+        for grade, students in duplicate_grades.items():
+            if len(students) == 2:
+                duplicate_messages.append(f"{students[0]} and {students[1]} have the same grade: {grade}%")
+            else:
+                duplicate_messages.append(
+                    f"{', '.join(students[:-1])}, and {students[-1]} have the same grade: {grade}%")
+
+        # Update duplicate grade label accordingly
+        if duplicate_messages:
+            self.duplicate_grade_label.setText("\n".join(duplicate_messages))
+        else:
+            self.duplicate_grade_label.setText("")
+
+        # Update labels for best and worst grades
         highest_index = grades.index(max(grades))
         lowest_index = grades.index(min(grades))
 
-        # Update the labels with the best and worst grades
         best_student = list(self.main_window.students.keys())[highest_index]
         worst_student = list(self.main_window.students.keys())[lowest_index]
 
